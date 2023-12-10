21eb1e74cef0f02a417c180970458b3197b3c7c8
lafonsecallorca <127278198+lafonsecallorca@users.noreply.github.com>
2023-12-09 23:05:36 -0500

    Update hourly_model.py

--- precipitation_prediction_model-main/hourly_model.py
+++ precipitation_prediction_model-main/hourly_model.py
@@ -13,87 +13,98 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.svm import SVR
 from weatherapi_class import WeatherData
 
-def read_and_clean_data(file_path):
+class HourlyModel:
+        
+    def __init__(self, file_path):
+            self.file_path = file_path
+            self.model = None
 
-    df = pd.read_csv(file_path, low_memory=False)
+    def read_and_clean_data(self):
 
-    df['p01i'] = df['p01i'].fillna(0)
+        df = pd.read_csv(self.file_path, low_memory=False)
 
-    old_cols = ['tmpf', 'valid', 'dwpf', 'relh', 'drct', 'sknt', 'alti', 'mslp', 'p01i', 'vsby']
-    new_cols = ['Temp (F)', 'Date', 'Dew Point', 'Humidity', 'Wind Direction', 'Wind Speed', 'Altimeter', 'Sea Level Pressure', 'Hourly Prcp', 'Visibility']
+        df['p01i'] = df['p01i'].fillna(0)
 
-    for old_col, new_col in zip(old_cols, new_cols):
-        df.rename(columns={old_col: new_col}, inplace=True)
+        old_cols = ['tmpf', 'valid', 'dwpf', 'relh', 'drct', 'sknt', 'alti', 'mslp', 'p01i', 'vsby']
+        new_cols = ['Temp (F)', 'Date', 'Dew Point', 'Humidity', 'Wind Direction', 'Wind Speed', 'Altimeter', 'Sea Level Pressure', 'Hourly Prcp', 'Visibility']
 
-    df['Temp (F)'] = df['Temp (F)'].interpolate()
-    df['Dew Point'] = df['Dew Point'].interpolate()
-    df['Humidity'] = df['Humidity'].interpolate()
-    df['Wind Direction'] = df['Wind Direction'].interpolate()
-    df['Wind Speed'] = df['Wind Speed'].interpolate()
-    #df['Altimeter'] = df['Altimeter'].interpolate()
-    df['Sea Level Pressure'] = df['Sea Level Pressure'].interpolate()
-    df['Hourly Prcp'] = df['Hourly Prcp'].interpolate()
-    df['Visibility'] = df['Visibility'].interpolate()
+        for old_col, new_col in zip(old_cols, new_cols):
+            df.rename(columns={old_col: new_col}, inplace=True)
 
-    #preparing data set for random forest regressor 
-    rfdf = df.copy()
-    rfdf = rfdf.drop(['station'], axis=1)
-    rfdf = rfdf.drop(['Date'], axis=1)
-    rfdf = rfdf.drop(['Altimeter'], axis=1)  #could not find API that gave altimeter so had to drop it from training feature
+        df['Temp (F)'] = df['Temp (F)'].interpolate()
+        df['Dew Point'] = df['Dew Point'].interpolate()
+        df['Humidity'] = df['Humidity'].interpolate()
+        df['Wind Direction'] = df['Wind Direction'].interpolate()
+        df['Wind Speed'] = df['Wind Speed'].interpolate()
+        #df['Altimeter'] = df['Altimeter'].interpolate()
+        df['Sea Level Pressure'] = df['Sea Level Pressure'].interpolate()
+        df['Hourly Prcp'] = df['Hourly Prcp'].interpolate()
+        df['Visibility'] = df['Visibility'].interpolate()
 
-    return rfdf
+        #preparing data set for random forest regressor 
+        rfdf = df.copy()
+        rfdf = rfdf.drop(['station'], axis=1)
+        rfdf = rfdf.drop(['Date'], axis=1)
+        rfdf = rfdf.drop(['Altimeter'], axis=1)  #could not find API that gave altimeter so had to drop it from training feature
 
-def split_and_scale_data(rfdf):
+        return rfdf
 
-    y = rfdf['Hourly Prcp']
-    X = rfdf.drop(['Hourly Prcp'],axis=1)
+    def split_and_scale_data(self, rfdf):
 
-    scaler = StandardScaler()  #z-score normilaztion worked best for large dataset
+        y = rfdf['Hourly Prcp']
+        X = rfdf.drop(['Hourly Prcp'],axis=1)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        scaler = StandardScaler()
 
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+        #z-score normilaztion worked best for large dataset
 
-    return X_train_scaled, X_test_scaled, y_train, y_test, scaler
-            
-def train_random_forest_model(X_train, y_train):
-    rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
-    rf_regressor.fit(X_train, y_train)
-    return rf_regressor
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-def train_knn_model(X_train, y_train):
-    knn = KNeighborsRegressor(n_neighbors=14)
-    knn.fit(X_train, y_train)
-    return knn
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
 
-def train_svr_model(X_train, y_train):
-    svr = SVR(kernel='rbf')
-    svr.fit(X_train, y_train)
-    return svr
+        return X_train_scaled, X_test_scaled, y_train, y_test, scaler
+                
+    def train_random_forest_model(self, X_train, y_train):
+        model = RandomForestRegressor(n_estimators=100, random_state=42)
+        model.fit(X_train, y_train)
+        return model
 
-def evaluate_model(model, X_test, y_test):
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    print(f'Mean Squared Error: {mse}')
-    print(f'R-squared: {r2}')
+    def train_knn_model(self, X_train, y_train):
+        self.model = KNeighborsRegressor(n_neighbors=14)
+        self.model.fit(X_train, y_train)
+        return self.model
+
+    def train_svr_model(self, X_train, y_train):
+        self.model = SVR(kernel='rbf')
+        self.model.fit(X_train, y_train)
+        return self.model
+
+    def evaluate_model(self, model, X_test, y_test):
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        print(f'Mean Squared Error: {mse}')
+        print(f'R-squared: {r2}')
+
+    def predict(self, model, data):
+        return model.predict(data)
+
 
 def main():
     file_path = 'ROC.csv'
-    df = read_and_clean_data(file_path)
+    weather_model = HourlyModel(file_path)
+    df = weather_model.read_and_clean_data()
 
-    X_train_scaled, X_test_scaled, y_train, y_test, scaler = split_and_scale_data(df)
+    X_train_scaled, X_test_scaled, y_train, y_test, scaler = weather_model.split_and_scale_data(df)
     
-    rf_model = train_random_forest_model(X_train_scaled, y_train)
-    rf_predict = rf_model.predict(X_test_scaled)
-
-    evaluate_model(rf_model, X_test_scaled, y_test)
-
-    knn_model = train_knn_model(X_train_scaled, y_train)
-    knn_predict = knn_model.predict(X_test_scaled)
-
-    evaluate_model(knn_model, X_test_scaled, y_test)
+    rf_model = weather_model.train_random_forest_model(X_train_scaled, y_train)
+    rf_predict = weather_model.predict(rf_model, X_test_scaled)
+    weather_model.evaluate_model(rf_model, X_test_scaled, y_test)
+    
+    knn_model = weather_model.train_knn_model(X_train_scaled, y_train)
+    knn_predict = weather_model.predict(knn_model, X_test_scaled)
+    weather_model.evaluate_model(knn_model, X_test_scaled, y_test)
 
     results_df = pd.DataFrame({
     'Actual_PRCP': y_test,
@@ -101,7 +112,7 @@ def main():
     'Predicted_RF': rf_predict
     })
 
-    # Display the DataFrame with actual and predicted values
+     # Display the DataFrame with actual and predicted values
     print(results_df.head(10))
     print(df.tail(5))
 
@@ -137,7 +148,7 @@ def main():
         current_weatherdf.rename(columns={api_col: model_col}, inplace=True)
 
     scaled_current_weatherdf = scaler.transform(current_weatherdf)
-    current_precipitation_predict = rf_model.predict(scaled_current_weatherdf)
+    current_precipitation_predict = weather_model.predict(rf_model,scaled_current_weatherdf)
     formatted_current_precipitation_predict = ['{:.2f}'.format(value) for value in current_precipitation_predict]
     formatted_current_precipitation_predict = formatted_current_precipitation_predict[0]
 
@@ -178,13 +189,16 @@ def main():
 
     scaled_fiveday_df = scaler.transform(fiveday_df_features)
 
-    fiveday_precipitation_predict = rf_model.predict(scaled_fiveday_df)
+    fiveday_precipitation_predict = weather_model.predict(rf_model, scaled_fiveday_df) 
+
+    print(fiveday_precipitation_predict)
 
     formatted_fiveday_precipitation_predict = ['{:.2f}'.format(value) for value in fiveday_precipitation_predict]
 
     fiveday_df['Predicted_Precipitation'] = formatted_fiveday_precipitation_predict
 
     print(fiveday_df)
+
         
 if __name__ == "__main__":
     main()
